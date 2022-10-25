41fdf7454e9ad8a22dc2f503c61183acaa1942ca
Chaltu Abdulkadir <chaltuabdulkadir@Chaltus-MacBook-Air.local>
2022-10-25 13:15:08 -0500

    initial Class and main method created

---
+++ NumberGussingGame.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class NumberGussingGame {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt(100) + 1;
+
+
+    }
+}
