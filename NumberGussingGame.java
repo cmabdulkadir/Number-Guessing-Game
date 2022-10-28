01b147d52198e3a21097fcb7396d179ac7ca7f80
Chaltu Abdulkadir <chaltuabdulkadir@Chaltus-MacBook-Air.local>
2022-10-28 13:32:00 -0500

    finialized game and udated readMe file

--- NumberGussingGame.java
+++ NumberGussingGame.java
@@ -7,18 +7,20 @@ public class NumberGussingGame {
 
         // declaring a new rand (object) this is built in Random class in java
         Random rand = new Random();
-
+        int tryCount = 0;
         //create variable to hold our random number = generating the random number that takes a range
         //gives you a range of numbers starting at 1 ending at 100
         int randomNumber = rand.nextInt(100) + 1;
 
+while (true){
         System.out.println("Enter your guess (1-100)");
-
         int playerGuess = scan.nextInt();
-
+            tryCount++
         if(playerGuess == randomNumber ){
 
             System.out.println("Correct! You Win");
+            System.out.println("It took you " + tryCount + " tries");
+            break;
 
         } else if (playerGuess > randomNumber) {
             System.out.println("Nope! The number is lower. Guess again");
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-## Number-Guessing-Game
+## Number Guessing Game
+
+In this game the computer will randomly pick a number between 1-100
+The user will be prompted to guess a number
+If the user is correct you win
+If the user is incorrect the computer will tell you if the random number is higher or lower than your guess.
+The user will continue guessing a number until they got it correct.
